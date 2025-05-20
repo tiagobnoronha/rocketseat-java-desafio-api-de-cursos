db21bfb001b25b7f3e991d4cad39976d8b0160bc
Tiago Baptista Noronha <tiago.bn@gmail.com>
2025-05-19 22:25:50 -0300

    first commit

---
+++ src/main/java/br/com/tiagobnoronha/rocket_api_curso/RocketApiCursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiagobnoronha.rocket_api_curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketApiCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketApiCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tiagobnoronha/rocket_api_curso/RocketApiCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiagobnoronha.rocket_api_curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketApiCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
